53f46fc9c8e1ad09493bafa736a9fc3a71d24c02
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-11-09 13:31:20 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

---
+++ JiWon/Week2/1463.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int D[n+1];
+    D[1] = 0;
+ 
+    for(int i = 2; i < n+1; i++){
+    /* 세 가지 방법 중 가장 적은 횟수를 사용하는 방법을 저장 */
+        D[i] = D[i-1] + 1;
+        if(i%2 == 0) D[i] = min(D[i], D[i/2]+1);
+        if(i%3 == 0) D[i] = min(D[i], D[i/3]+1);
+    }
+
+    cout << D[n];
+    return 0;
+}
