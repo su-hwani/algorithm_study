85338602d2c55a11c892383ea51d97028c02f68a
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-11-14 15:56:05 +0900

    [Gold V] Title: 호텔, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- JiWon/Week2/1106.cc
+++ JiWon/Week2/1106.cc
@@ -4,28 +4,27 @@
 using namespace std;
 
 int main(){
-    int c, n; // C: 늘여야 하는 최소 고객 수(~1000),  N: 홍보할 수 있는 도시 수(~20)
-    cin >> c >> n;
-    
+    int c, n; // C는 1000보다 작거나 같은 자연수,  N은 20보다 작거나 같은 자연수
+    int cost, num; // 100보다 작거나 같은 자연수
+
     vector<pair<int, int>> list;
     const int MAX = 1000*100 + 1; // C의 최댓값 * 입력 cost의 최댓값 + 1
     int D[MAX]= {}; // D[cost] = num
-
+    
+    cin >> c >> n;
     for(int i=0; i<n; i++){
-        // 도시별 홍보 정책 저장
-        int cost, num;
         cin >> cost >> num;
         list.push_back(make_pair(cost, num));
     }
-
-    for(int i=1; i<MAX; i++){
-        for(int j=0; j<n; j++){
+    
+    for(int i=0; i<MAX; i++){
+        for(int j=0; j<n; j++){ // 각각의 cost에 대해 모든 정책을 검토
             if(i - list[j].first >= 0){
                 D[i] = max(D[i], D[i-list[j].first] + list[j].second);
             }
         }
-        if(D[i] >= c){ // 최소치 달성하면 cost 출력
-            cout << i;
+        if(D[i] >= c){
+            cout << i; // 최소치 달성하면 cost 출력
             break;
         }
     }
