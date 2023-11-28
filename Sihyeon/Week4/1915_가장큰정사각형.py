c0b5f2cd20a8e58081b38b38cdab25c24fb5a71f
shyun <68101236+dkstlgus7571@users.noreply.github.com>
2023-11-28 18:22:52 +0900

    sihyun 수정

--- Sihyeon/Week4/1915_가장큰정사각형.py
+++ Sihyeon/Week4/1915_가장큰정사각형.py
@@ -8,15 +8,11 @@ for i in range(n):
 
 answer = 0
 
-# 배열이 1x1인 경우
-if n == 1 and m == 1:
-    answer = arr[0][0]
-else:
-    # arr 요소 중 값이 1일 때 arr의 전행, 전열, 전행전열 비교해서 가장 작은 것에서 1을 더한다.
-    for i in range(1, n):
-        for j in range(1, m):
-            if arr[i][j] == 1:
-                arr[i][j] = min(arr[i-1][j], arr[i][j-1], arr[i-1][j-1]) + 1
-                answer = max(answer, arr[i][j])
+# arr 요소 중 값이 1일 때 arr의 전행, 전열, 전행전열 비교해서 가장 작은 것에서 1을 더한다.
+for i in range(n):
+    for j in range(m):
+        if n > 1 and m > 1 and arr[i][j] == 1:
+            arr[i][j] = min(arr[i-1][j], arr[i][j-1], arr[i-1][j-1]) + 1
+        answer = max(answer, arr[i][j])
 
 print(answer * answer)
