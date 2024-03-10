f0b13798ed2fc7ff6a78b859af49bf708025a01b
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-03-10 10:26:47 +0900

    Create 11382.java

---
+++ JongBin/13week/11382.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long answer = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        answer = a + b + c;
+        System.out.println(answer);
+    }
+}
