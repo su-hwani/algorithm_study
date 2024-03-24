584afc6a05deb113ba6025513168472a4040e256
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-03-24 11:48:41 +0900

    Create 1806.java

---
+++ JongBin/막 푼것/1806.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int total = 0;
+        while (start <= N && end <= N) {
+            if (total >= M && min > end - start) min = end - start;
+            if (total < M) total += arr[end++];
+            else total -= arr[start++];
+        }
+        if (min == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(min);
+        }
+    }
+}
