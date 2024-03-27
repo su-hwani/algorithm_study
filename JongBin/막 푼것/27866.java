116571fbc6546dc866c293c81fa4d6410f57c5fb
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-03-27 15:58:47 +0900

    Create 27866.java

---
+++ JongBin/막 푼것/27866.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        System.out.println(s.charAt(i-1));
+    }
+}
