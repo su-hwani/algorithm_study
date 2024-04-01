8b7f95df65bd77b0a9c3c0727992ae0f88d2a649
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-04-01 21:04:59 +0900

    Create 27323.java

---
+++ JongBin/막 푼것/27323.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        System.out.println(N*M);    
+    }    
+}
