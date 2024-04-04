970d239c2b468429a16d55b5b8f87931858f4bc5
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-04-04 10:42:06 +0900

    Create 14645.java

---
+++ JongBin/막 푼것/14645.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br. readLine());
+        int N = Integer.parseInt(st.nextToken());
+        for(int i =0;i<N;i++){
+            br.readLine();
+        }
+        System.out.println("비와이");
+
+    }
+}
