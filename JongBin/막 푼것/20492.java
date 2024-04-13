d1cabeb94a8c50918d388f5ca4530b3c60fd722c
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-04-13 15:15:32 +0900

    Create 20492.java

---
+++ JongBin/막 푼것/20492.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int)(N*(1-0.22))+" "+(int)((N-N*0.2*0.22)));
+    }
+}
