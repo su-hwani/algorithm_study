336c28b0552971155982f0b225c63a44e2dd57bd
su-hwani <wjdtnghks123@kookmin.ac.kr>
2025-01-21 20:19:50 +0900

    [Silver III] Title: N과 M (3), Time: 1100 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+            
+
+n, m = map(int, input().split())
+array = []
+
+def back():
+    if len(array) == m:
+        print(' '.join(map(str, array)))
+        return 
+    
+    for i in range(1, n+1):
+        # if len(array) > 0 and array[-1] > i:
+        #     continue
+        # if i not in array:
+        #     array.append(i)
+        #     back()
+        #     array.pop()
+        array.append(i)
+        back()
+        array.pop()
+            
+back()
