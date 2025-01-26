a51d178062682435267c5ecb84de0689d8c906ab
su-hwani <wjdtnghks123@kookmin.ac.kr>
2025-01-26 16:47:27 +0900

    [Silver II] Title: 나무 자르기, Time: 4880 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,23 +1,27 @@
-n, m = map(int, input().split())
-tree = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-# 이진 탐색을 위한 시작점, 끝점
+N, M = map(int, input().split())
+
+nums = list(map(int, input().split()))
+
 start = 0
-end = max(tree)
+end = max(nums)
+result = 0
 
-result = 0 # 최종 절단기 높이
-while(start <= end):
-    total = 0 # 가져갈 나무의 길이
-    mid = (start + end) // 2 # 절단기 높이
-    for i in tree:
-        if i > mid: # 절단기보다 주어진 나무가 길면 자른 나머지를 total에 추가
-            total += i - mid
-            
-    # 이진 탐색        
-    if total < m: # 가져가야 하는 길이 m보다 작은 길이이면
-        end = mid - 1 # mid 기준 왼쪽 부분 탐색
-    else: # 가져가야 하는 길이 m보다 크거나 같으면
-        result = mid # 절단기 높이를 mid로 설정
-        start = mid + 1 # mid 기준 오른쪽 부분 탐색
+while start <= end:
+    mid = (start + end) // 2
+    count = 0
+    for i in nums:
+        if i > mid:
+            count += (i-mid)
 
+    if count >= M:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+    
+    # print(f"start: {start}, end: {end}, mid: {mid}, ans: {count}")
+    
 print(result)
