b4f451b1e9494d43e71e97f2b3a13adbba2ad568
su-hwani <wjdtnghks123@kookmin.ac.kr>
2025-03-02 20:22:34 +0900

    [level 3] Title: 섬 연결하기, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -3,36 +3,24 @@ def find(parent, x):
         parent[x] = find(parent, parent[x])
     return parent[x]
 
-def union(parent, rank, a, b):
-    rootA = find(parent, a)
-    rootB = find(parent, b)
+def union(parent, u, v):
+    rootA = find(parent, u)
+    rootB = find(parent, v)
     
     if rootA != rootB:
-        if rank[rootA] > rank[rootB]:
-            parent[rootB] = rootA
-        elif rank[rootA] < rank[rootB]:
-            parent[rootA] = rootB
-        else:
-            parent[rootB] = rootA
-            rank[rootA] += 1
-            
-            
-    
+        parent[rootA] = rootB
 
 def solution(n, costs):
-    
-    edges = sorted(costs, key=lambda x: x[2])
-    
+    edges = sorted(costs, key=lambda x:x[2])
     parent = [i for i in range(n+1)]
-    rank = [0] * n
-    
-    mst_cost = 0
     mst_edges = []
+    mst_cost = 0
     
     for u, v, cost in edges:
         if find(parent, u) != find(parent, v):
-            union(parent, rank, u, v)
+            union(parent, u, v)
             mst_cost += cost
             mst_edges.append( (u, v, cost) )
     
-    return mst_cost
+    answer = mst_cost
+    return answer
