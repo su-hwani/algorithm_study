5b25e7ef3397c8f26bad30cf75c2cd4308bb16d0
su-hwani <wjdtnghks123@kookmin.ac.kr>
2025-12-22 22:03:49 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,22 +1,19 @@
 import sys
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+input = sys.stdin.readline 
+
+
+
 
 
 N = int(input())
 
-numbers = list(map(int, input().split()))
-graph = [1 for _ in range(N)]
+graph = list(map(int, input().split()))
+dp = [1 for _ in range(N)]
 
+for i in range(N):
+    for k in range(i):
+        if graph[i] > graph[k]:
+            dp[i] = max(dp[i], dp[k] + 1)
 
-for i in range(1, N):
-    
-    for k in range(0, i):
-        if numbers[i] > numbers[k]:
-            graph[i] = max(graph[i], graph[k]+1)
-    
-
-print(max(graph))
-
-#print(graph)
+print(max(dp))
